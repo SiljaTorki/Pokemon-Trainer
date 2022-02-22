be2c0a0c013d90c27dbcb8e8d249d7ee0f7614fe
SiljaTorki <silja.storkildsen@gmail.com>
2022-02-22 01:18:51 +0100

    JSON

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('./dist/package.json'));
 
 
 app.get('', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
+    res.sendFile('index.html', {root: 'dist/package.json/'}
   );
 });
 
